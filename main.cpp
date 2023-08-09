f57e0997296e160e7e31fd35fa81c31693e2ab10
HFcqw <98407029+HFcqw@users.noreply.github.com>
2023-08-09 16:17:47 +0800

    修改

--- main.cpp
+++ main.cpp
@@ -1,5 +1,28 @@
-#include"test.h"
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<cstdio>
+#include<graphics.h>
+#include<conio.h>
+using namespace std;
+
 int main() {
-	cout << "Hello World!" << endl;
+	initgraph(1920, 1080,SHOWCONSOLE);
+	setbkcolor(WHITE);
+	cleardevice();
+	IMAGE mage;
+	loadimage(&mage, "D://图片//Camera Roll//550W.jpg");
+	putimage(0, 0, &mage);
+	/*
+	settextcolor(BLACK);
+	settextstyle(20, 0, "楷体");
+	outtextxy(100, 100, "你好，世界");
+
+	setlinecolor(BLACK);
+	setfillcolor(BLUE);
+	circle(50, 50, 50);
+	fillcircle(50, 150, 50);
+	*/
+	getchar();
+	closegraph();
 	return 0;
 }
